0fb1459df34e69c30c4a2909c82d02b2d5966876
ymogg <rusfoot.2021@gmail.com>
2026-02-13 21:15:34 +0300

    1.2

--- common/defines/defines.lua
+++ common/defines/defines.lua
@@ -1,14 +1,14 @@
 NDefines.NProduction.BASE_FACTORY_SPEED = 12
 NDefines.NProduction.BASE_FACTORY_SPEED_MIL = 8
-NDefines.NProduction.BASE_FACTORY_EFFICIENCY_GAIN = 1.4
-NDefines.NProduction.BASE_FACTORY_MAX_EFFICIENCY_FACTOR = 60
-NDefines.NProduction.BASE_FACTORY_START_EFFICIENCY_FACTOR = 50
+NDefines.NProduction.BASE_FACTORY_EFFICIENCY_GAIN = 1.2
+NDefines.NProduction.BASE_FACTORY_MAX_EFFICIENCY_FACTOR = 60 -- доп макса 
+NDefines.NProduction.BASE_FACTORY_START_EFFICIENCY_FACTOR = 40 -- старт эффективность крафта
 
-NDefines.NPolitics.BASE_POLITICAL_POWER_INCREASE = 10
+NDefines.NPolitics.BASE_POLITICAL_POWER_INCREASE = 10 -- прирост политки
 
-NDefines.NTechnology.BASE_YEAR_AHEAD_PENALTY_FACTOR = 2
-NDefines.NTechnology.BASE_TECH_COST = 25
-NDefines.NTechnology.MIN_RESEARCH_SPEED = 0.8
+NDefines.NTechnology.BASE_YEAR_AHEAD_PENALTY_FACTOR = 2 -- штраф за опережение
+NDefines.NTechnology.BASE_TECH_COST = 25 -- снижение коста ресерчей
+NDefines.NTechnology.MIN_RESEARCH_SPEED = 0.8 -- скорость исследования
 
 NDefines.NDiplomacy.WARGOAL_PER_JUSTIFY_AND_WAR_COST_FACTOR = 1.5
 
@@ -22,26 +22,31 @@ NDefines.NMilitary.FIELD_EXPERIENCE_MAX_PER_DAY = 15
 NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 9999
 NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 9999
 NDefines.NMilitary.MAX_AIR_EXPERIENCE  = 9999
-NDefines.NMilitary.TRAINING_EXPERIENCE_SCALE = 85.0
+NDefines.NMilitary.TRAINING_EXPERIENCE_SCALE = 85.0 -- ускорение тренировки дивизий
 NDefines.NMilitary.UNIT_EXPERIENCE_PER_TRAINING_DAY = 1
-NDefines.NMilitary.BASE_ARMY_EXPERIENCE_GAIN = 1
-NDefines.NMilitary.BASE_LEADER_TRAIT_GAIN_XP = 1
-NDefines.NMilitary.UNIT_LEADER_USE_NONLINEAR_XP_GAIN = false
+NDefines.NMilitary.BASE_ARMY_EXPERIENCE_GAIN = 1 -- ускорение прокачки перков гена
+NDefines.NMilitary.BASE_LEADER_TRAIT_GAIN_XP = 1 -- увеличение опыта гена за бои
+NDefines.NMilitary.UNIT_LEADER_USE_NONLINEAR_XP_GAIN = false -- откл снижения прокачки трейтов при условии что есть другие
+NDefines.NMilitary.RIVER_CROSSING_PENALTY = -0.1 -- штраф за малую реку
+NDefines.NMilitary.RIVER_CROSSING_PENALTY_LARGE = -0.25 -- штраф за большую реку
+NDefines.NCombat = { -- штрафы за бункера
+    FORT_PENALTY = -0.08,
+    COASTAL_FORT_PENALTY = -0.08
+}
 
+NDefines.NAir.AIR_DEPLOYMENT_DAYS = 0 -- время развертки авиации
+NDefines.NAir.AIR_REBASE_TIME_FACTOR = 0 -- время переброски авиации
 NDefines.NAir.FIELD_EXPERIENCE_SCALE = 0.01
 NDefines.NAir.FIELD_EXPERIENCE_MAX_PER_DAY = 5
-
 NDefines.NNavy.UNIT_EXPERIENCE_PER_COMBAT_HOUR = 20
 NDefines.NNavy.UNIT_EXPERIENCE_SCALE = 3
 NDefines.NNavy.FIELD_EXPERIENCE_SCALE = 0.75
 NDefines.NNavy.FIELD_EXPERIENCE_MAX_PER_DAY = 100
-NDefines.NNavy.NAVAL_INVASION_PREPARE_HOURS = 0
 -- NDefines.NNavy.AMPHIBIOUS_INVADE_SPEED_BASE = 10
-NDefines.NNavy.NAVAL_SPEED_MODIFIER = 0.25
 NDefines.NNavy.NAVAL_TRANSFER_BASE_SPEED = 5
 
-NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 999
-NDefines.NGame.LAG_DAYS_FOR_PAUSE = 10
+NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 999 -- дней до снижение скорости за отставание
+NDefines.NGame.LAG_DAYS_FOR_PAUSE = 10 -- дней для снятия паузы от человека
 
 NDefines.NCountry.SUPPLY_BASE_MULT = 3
 
